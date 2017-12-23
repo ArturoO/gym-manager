dffaa86a93a6db2d59caebb5252e95fb8e96b03a
Artur Hoffman <thearturoo87@gmail.com>
2017-12-23 21:24:22 +0100

    Remove redundant code. After creating an event redirect to the events list and also display notification.

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -16,53 +16,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EventController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-		
-        return $this->render('default/index.html.twig', array(
-            
-        ));
-    }
-	
-	/**
-     * @Route("/phpinfo", name="php_info")
-     */
-    public function phpInfoAction(Request $request)
-    {
-		ob_start();
-		phpinfo();
-		$phpinfo = ob_get_clean();
-		
-        return $this->render('default/phpinfo.html.twig', array(
-			'phpinfo' => $phpinfo,
-		));
-    }
-	
-	
-	 /**
-     * @Route("/create/product", name="create_product")
-     */
-    public function createProductAction(Request $request)
-    {
-        $product = new Product();
-		$product->setName('Keyboard');
-		$product->setPrice(19.99);
-		$product->setDescription('Ergonomic and stylish!');
-
-		$em = $this->getDoctrine()->getManager();
-
-		// tells Doctrine you want to (eventually) save the Product (no queries yet)
-		$em->persist($product);
-
-		// actually executes the queries (i.e. the INSERT query)
-		$em->flush();
-
-		return new Response('Saved new product with id '.$product->getId());
-    }
-	
 	/**
      * @Route("/event/create", name="create_event")
      */
@@ -79,6 +32,13 @@ class EventController extends Controller
 			$em = $this->getDoctrine()->getManager();
 			$em->persist($Event);
 			$em->flush();
+			
+			//add flash message and redirect to event list
+			$this->addFlash(
+				'event-notice',
+				'Event has been added'
+			);
+			return $this->redirectToRoute('view_events');
 		}
 		
         return $this->render('event/create.html.twig', array(			
@@ -98,46 +58,5 @@ class EventController extends Controller
         ));
 	}
 	
-	/**
-     * @Route("/task/create", name="create_task")
-     */
-    public function createTaskAction(Request $request)
-    {
-        $task = new Task();
-        $form = $this->createFormBuilder($task)
-            ->add('task', TextType::class)            
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, array('label' => 'Create Task'))
-            ->getForm();
-		
-		$form->handleRequest($request);
-
-		if($form->isSubmitted() && $form->isValid()) {
-			$task2 = $form->getData();
-			
-			$em = $this->getDoctrine()->getManager();
-			$em->persist($task2);
-			$em->flush();
-		}
-		
-		$repository = $this->getDoctrine()->getRepository(Task::class);
-		$tasks = $repository->findBy(array(), array('dueDate' => 'desc'));
-//		$tasks = $repository->findAll();
-		
-        return $this->render('default/create-task.html.twig', array(
-			'tasks' => $tasks,
-            'form' => $form->createView(),
-        ));
-	}
-	
-	/**
-     * @Route("/task/success", name="task_success")
-     */
-    public function taskSuccessAction(Request $request)
-    {
-		return new Response(
-            '<html><body>Task has been added ! </body></html>'
-        );
-	}
 	
 }
