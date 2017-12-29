9cc28718e0022cffb812cb6492550c9ffadf6d31
Artur Hoffman <thearturoo87@gmail.com>
2017-12-29 20:47:13 +0100

    Add support for controls that expand and collapse area.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,11 +19,11 @@ class DefaultController extends Controller
 		
 		$collapsibleAreaArray = array(
 			array(
-				'collapse' => 0,
+				'collapse' => 1,
 				'content' => 'Sed sagittis nunc diam, mattis molestie neque vehicula eu. In ut eleifend nisl. Mauris eu nisl ultricies, auctor lacus in, suscipit leo. Suspendisse at lacus sit amet lacus scelerisque congue vel ac ex. Etiam eget orci pharetra, egestas velit sed, pharetra tellus. Duis rhoncus nisl vel luctus auctor. Nunc porttitor, risus ultricies egestas faucibus, lacus eros eleifend justo, at mattis turpis dui lobortis tellus. ',
 			),
 			array(
-				'collapse' => 1,
+				'collapse' => 0,
 				'content' => 'Fusce finibus diam ut nunc fermentum elementum. Sed lectus arcu, elementum volutpat ligula ac, vulputate scelerisque risus. Etiam vitae tellus eleifend, lacinia ante in, auctor dolor. Nam tempus fermentum ante pretium commodo. Duis ut imperdiet dolor. Nunc ullamcorper, velit eget maximus sodales, lectus ligula vulputate augue, quis congue massa risus vel turpis. Aenean id sagittis metus, in accumsan augue. Pellentesque feugiat eleifend bibendum. ',
 			),
 		);
--- web/js/jquery.collapsible-area.js
+++ web/js/jquery.collapsible-area.js
@@ -1,10 +1,9 @@
 (function($) {
 	
-	
 	function CollapsibleArea(objects)
 	{
-		var options;
-		this.$objects = $(objects);	//array or objects, not a single object
+//		this.$objects = $(objects);
+		this.$objects = objects;
 	}
 	
 	CollapsibleArea.prototype.init = function()
@@ -14,23 +13,51 @@
 			if(this.$objects.eq(i).attr('data-collapse')==1)
 			{
 				this.collapse(this.$objects.eq(i));
+//				this.$objects.eq(i).css('height', '45px').addClass('ca-state-collapsed');
 			}
 		}
+		
+		//assign handlers
+		this.$objects.find('.ca-control-collapse').on('click', this.collapseClick);		
+		this.$objects.find('.ca-control-expand').on('click', this.expandClick);		
+	}
+	
+	CollapsibleArea.prototype.collapseClick = function()
+	{
+		CollapsibleArea.prototype.collapse($(this).closest('.collapsible-area'));
 	}
 	
 	CollapsibleArea.prototype.collapse = function($el)
 	{
 		$el.animate(
-			{'height': '10px'},
-			200
+			{'height': '45px'},
+			200,
+			'swing',
+			function() {
+				$el.addClass('ca-state-collapsed');
+			}
 		);
+	}
+	
+	CollapsibleArea.prototype.expandClick = function()
+	{
+		CollapsibleArea.prototype.expand($(this).closest('.collapsible-area'));
 	}
 	
 	CollapsibleArea.prototype.expand = function($el)
 	{
+		var currHeight = $el.css('height');
+		$el.css('height', 'auto');
+		var fullHeight = $el.css('height');
+		$el.css('height', currHeight);
+		
 		$el.animate(
-			{'height': ''},
-			200
+			{'height': fullHeight},
+			200,
+			'swing',
+			function() {
+				$el.removeClass('ca-state-collapsed');
+			}
 		);
 	}
 	
