a3ab49711baa7aa9b2228eb83d27290c3a572967
Artur Hoffman <thearturoo87@gmail.com>
2018-01-27 14:34:47 +0100

    Create login form and handle login process.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends Controller
 {
@@ -19,6 +20,23 @@ class UserController extends Controller
 		return $this->render(
             'user/list.html.twig'
         );
+	}
+	
+	/**
+     * @Route("/login", name="login")
+     */
+	public function loginAction(Request $request, AuthenticationUtils $authUtils)
+	{
+		// get the login error if there is one
+		$error = $authUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authUtils->getLastUsername();
+
+		return $this->render('user/login.html.twig', array(
+			'last_username' => $lastUsername,
+			'error'         => $error,
+		));
 	}
 	
     /**
