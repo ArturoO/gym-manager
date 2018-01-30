fc387bda5790600dd95894f7cefa001628ff6ce7
Artur Hoffman <thearturoo87@gmail.com>
2018-01-30 19:54:08 +0100

    [minor]Format the code.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -115,10 +115,6 @@ class UserController extends Controller
 			])
 			->getForm();
 		
-		
-//        $user = new User();
-//        $form = $this->createForm(UserType::class, $user);
-
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -129,17 +125,10 @@ class UserController extends Controller
 				->setRole($formData['role'])				
 			;
 			
-            // 3) Encode the password (you could also do this via Doctrine listener)
-//            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-//            $user->setPassword($password);
-
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
 
             return $this->redirectToRoute('users_list');
         }
